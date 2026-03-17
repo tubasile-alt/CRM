6c445aba1c1b696a83db58f1c59c4c3028d1eae3
tubasile-alt <tubasile@gmail.com>
2026-03-17 15:56:51 +0000

    Restore 'Mark as performed' button and fix procedure_execution inconsistency
    - Added green checkmark button to mark pending procedures as performed
    - Fixed 11 procedures with was_performed=True but performed_date=NULL by setting date to today
    - Button appears only for pending procedures (not realized ones)
    - Includes date picker for procedure execution
    - Fixed Roberto Jabali Botox procedures showing correct status

--- static/js/prontuario.js
+++ static/js/prontuario.js
@@ -951,9 +951,14 @@ function renderProcedureCard(proc) {
               </div>
             </div>
           </div>
-          <button class="btn btn-sm btn-outline-danger py-1 px-2" title="Deletar planejamento" onclick="deleteCosmeticPlan(${proc.id}, '${core.escapeHtml(proc.name)}')">
-            <i class="bi bi-trash"></i>
-          </button>
+          <div class="d-flex gap-1">
+            ${!performed ? `<button class="btn btn-sm btn-outline-success py-1 px-2" title="Marcar como realizado" onclick="performCosmeticProcedure(${proc.id}, '${core.escapeHtml(proc.name)}')">
+              <i class="bi bi-check-lg"></i>
+            </button>` : ''}
+            <button class="btn btn-sm btn-outline-danger py-1 px-2" title="Deletar planejamento" onclick="deleteCosmeticPlan(${proc.id}, '${core.escapeHtml(proc.name)}')">
+              <i class="bi bi-trash"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
